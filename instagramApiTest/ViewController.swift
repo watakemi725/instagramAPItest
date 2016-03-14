5dfe31483abee3b4d9d7b353d241f61cd25cf9bb
Takemi Watanuki <takemi007@hotmail.com>
2016-03-15 01:43:11 +0900

    first commit

---
+++ instagramApiTest/ViewController.swift
@@ -0,0 +1,39 @@
+//
+//  ViewController.swift
+//  instagramApiTest
+//
+//  Created by Takemi Watanuki on 2016/02/11.
+//  Copyright © 2016年 Takemi Watanuki. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        
+        //instagramAPIを用いてアプリを開発
+        /*
+        
+        その人の投稿写真をいいねが多い写真順にならべることや
+        
+        その人の写真んを投稿順に〜〜
+            
+        
+        
+        */
+        
+        
+        
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    
+}
+
